80f80870996bec1acd9c06c61e6de89229315f5a
mandar2n <kimkyuree1@naver.com>
2023-08-08 17:27:34 +0900

    practicing spring framework_studied DB approach

---
+++ src/main/java/kyul/springstudy/aop/TimeTraceAop.java
@@ -0,0 +1,29 @@
+package kyul.springstudy.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class TimeTraceAop {
+
+    @Around("execution(* kyul.springstudy..*(..))") // 공통관심사 적용될 부분 타켓화
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable{
+
+        long start = System.currentTimeMillis();
+
+        System.out.println("START: " + joinPoint.toString());
+
+        try{
+            return joinPoint.proceed();
+        } finally{
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+
+}
--- src/main/java/kyul/springstudy/service/SpringConfig.java
+++ src/main/java/kyul/springstudy/service/SpringConfig.java
@@ -1,5 +1,6 @@
 package kyul.springstudy.service;
 
+import kyul.springstudy.aop.TimeTraceAop;
 import kyul.springstudy.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -45,5 +46,10 @@ public class SpringConfig {
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
         //return new JpaMemberRepository(em);
+    }*/
+
+    /*@Bean
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
     }*/
 }
